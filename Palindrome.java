0e09b5ea5bbc0e9047b318dfb566585066f9a2b5
myFirst <mohdmzml1001@gmail.com>
2023-09-18 14:39:24 +0530

    adding new program

---
+++ Palindrome.java
@@ -0,0 +1,27 @@
+package college;
+
+import java.util.*;
+
+public class Palindrome {
+    public void pali() {
+        Scanner wrd = new Scanner(System.in);
+        String word = wrd.nextLine();
+        StringBuilder s = new StringBuilder();
+        s.append(word);
+        s.reverse();
+        System.out.println(s);
+        if (word.equals(s.toString())) {
+            System.out.println(s + "!" + " its a palindrome");
+        } else {
+            System.out.println(s + "!" + " is not a palindrome");
+        }
+
+        wrd.close();
+    }
+
+    public static void main(String[] args) {
+        Palindrome obj = new Palindrome();
+        obj.pali();
+
+    }
+}
